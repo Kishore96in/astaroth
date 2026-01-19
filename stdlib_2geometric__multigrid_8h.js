99f39e41bfaa6f3c484d7aa8fc49f8a3a5d3ba6f
ToxPuro <ToxPuro@users.noreply.github.com>
2026-01-19 10:50:02 +0000

    deploy: 7127f4d0605ad2c3cc39ea7c56828773596d2dfe

--- stdlib_2geometric__multigrid_8h.js
+++ stdlib_2geometric__multigrid_8h.js
@@ -2,12 +2,12 @@ var stdlib_2geometric__multigrid_8h =
 [
     [ "get_galerkin_operator", "stdlib_2geometric__multigrid_8h.html#a43c436c2644c0676c340802eae669189", null ],
     [ "get_galerkin_operators", "stdlib_2geometric__multigrid_8h.html#a8bb46fe317ddc27b5b2d76aa45c5aeec", null ],
-    [ "gmg_level_step", "stdlib_2geometric__multigrid_8h.html#a2b9849d358bde7c21426e4565800a3f3", null ],
+    [ "gmg_level_step", "stdlib_2geometric__multigrid_8h.html#a982001c00d7cd02c44d2bb66d06b0545", null ],
     [ "gmg_populate_central_coeffients", "stdlib_2geometric__multigrid_8h.html#a3d7e20b0e945c3cec3c44f0e73fb698c", null ],
     [ "gmg_restrict_to_level", "stdlib_2geometric__multigrid_8h.html#ab1d5c3c1675f25314544bb4963591eb8", null ],
     [ "gmg_setup", "stdlib_2geometric__multigrid_8h.html#a4b084b0a43134ea36afd9eb3e718ba13", null ],
     [ "gmg_store_and_prolong", "stdlib_2geometric__multigrid_8h.html#a80f85e3438b9a41792fcb9337356cbdd", null ],
-    [ "gmg_v_cycle", "stdlib_2geometric__multigrid_8h.html#a54b4a59009ee906b17a971df92cc8d1d", null ],
+    [ "gmg_v_cycle", "stdlib_2geometric__multigrid_8h.html#ad1cfac260bde0217d59097d867454c0b", null ],
     [ "gmg_central_coeffs", "stdlib_2geometric__multigrid_8h.html#a4ca23d473f99a5c70ebcc94484683fb9", null ],
     [ "level_dims", "stdlib_2geometric__multigrid_8h.html#aba2a73a8d93be9be04cd3aacc4220ec9", null ]
 ];
