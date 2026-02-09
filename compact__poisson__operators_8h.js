514ce06531b9ecd25976d57c252f47e42b7747ca
ToxPuro <ToxPuro@users.noreply.github.com>
2026-02-09 20:05:19 +0000

    deploy: accc6666a41833c910247ba2565210a2104c9381

---
+++ compact__poisson__operators_8h.js
@@ -0,0 +1,15 @@
+var compact__poisson__operators_8h =
+[
+    [ "COMPACT_POISSON_2ND_ORDER_CENTRAL", "compact__poisson__operators_8h.html#a64ce7bb552c8dc137224ffcffef3037b", null ],
+    [ "COMPACT_POISSON_2ND_ORDER_CUBICAL", "compact__poisson__operators_8h.html#a6f40eb12420fd87d7db23a95e277f6c7", null ],
+    [ "COMPACT_POISSON_2ND_ORDER_DIAGONAL", "compact__poisson__operators_8h.html#aa7771cd2f639cf49c8181c984059f655", null ],
+    [ "COMPACT_POISSON_2ND_ORDER_FACE", "compact__poisson__operators_8h.html#aa2d4bae67c3813acc88bb1f024d009da", null ],
+    [ "COMPACT_POISSON_4TH_ORDER_CENTRAL", "compact__poisson__operators_8h.html#af5f1e38789faea7dc8d4c214c575fddb", null ],
+    [ "COMPACT_POISSON_4TH_ORDER_CUBICAL", "compact__poisson__operators_8h.html#affb74e655df7aed36c20b273b73b4c06", null ],
+    [ "COMPACT_POISSON_4TH_ORDER_DIAGONAL", "compact__poisson__operators_8h.html#a72b00498226f4799e54c5bb7088c23a8", null ],
+    [ "COMPACT_POISSON_4TH_ORDER_FACE", "compact__poisson__operators_8h.html#accde1279d510bbb0f2746fb3435044d1", null ],
+    [ "COMPACT_POISSON_6TH_ORDER_CENTRAL", "compact__poisson__operators_8h.html#a7f9e07a5e43a47878a1b3b8e8741976d", null ],
+    [ "COMPACT_POISSON_6TH_ORDER_CUBICAL", "compact__poisson__operators_8h.html#af37e8e6518cab9de7473390c1bb95426", null ],
+    [ "COMPACT_POISSON_6TH_ORDER_DIAGONAL", "compact__poisson__operators_8h.html#ade17369271ca25b4d1d356c5d2f58558", null ],
+    [ "COMPACT_POISSON_6TH_ORDER_FACE", "compact__poisson__operators_8h.html#a3c52c0a1b3967e79e36a8520078e5f1e", null ]
+];
