817b044e4e827f69b9a232220b720282c2eabc37
ToxPuro <ToxPuro@users.noreply.github.com>
2026-04-14 16:20:52 +0000

    deploy: c3a7c6fbab03d3b8f4f02d6de17b2518dfe2df51

--- nofft_8cc.js
+++ nofft_8cc.js
@@ -8,7 +8,7 @@ var nofft_8cc =
     [ "acFFTForwardTransformR2C", "nofft_8cc.html#abb2b9d1dd3190c72c40735210234b159", null ],
     [ "acFFTForwardTransformR2HermitianPlanarBatched", "nofft_8cc.html#a9e81aa0c124fb0e5b26f9c9f99651b42", null ],
     [ "acFFTForwardTransformR2Planar", "nofft_8cc.html#aa43ffc9e078a843e3ea0e695043c77f3", null ],
-    [ "acFFTForwardTransformR2PlanarBatched", "nofft_8cc.html#a9fc33d4dd1b13fc7d904a17b831ccef5", null ],
+    [ "acFFTForwardTransformR2PlanarBatched", "nofft_8cc.html#adcc56336fbbfee97b535371e5eafd7ee", null ],
     [ "acFFTForwardTransformSymmetricR2C", "nofft_8cc.html#a21875adbe4cd1dce358298978b232b66", null ],
     [ "acFFTInit", "nofft_8cc.html#a230cf6f596266b47aa5351443e72cbfb", null ],
     [ "acFFTQuit", "nofft_8cc.html#abd575c60198344f9c50fd0df51fe2392", null ]
