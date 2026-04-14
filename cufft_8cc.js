3204fc3f8602331628d131593a2fd0d94f6fc3fe
ToxPuro <ToxPuro@users.noreply.github.com>
2026-04-14 18:42:34 +0000

    deploy: 3721c1e858b25e2a79294ce09490bc58f76624bc

--- cufft_8cc.js
+++ cufft_8cc.js
@@ -13,7 +13,7 @@ var cufft_8cc =
     [ "acFFTForwardTransformR2C", "cufft_8cc.html#a4a2c38a3c895e671102a9af8712e208f", null ],
     [ "acFFTForwardTransformR2HermitianPlanarBatched", "cufft_8cc.html#afaae509b335347ed98de23de6a54fbf8", null ],
     [ "acFFTForwardTransformR2Planar", "cufft_8cc.html#a5b6e24381dd03553e89c1aa8fb441160", null ],
-    [ "acFFTForwardTransformR2PlanarBatched", "cufft_8cc.html#a5552601650346f7b9c62162dc7d74fbf", null ],
+    [ "acFFTForwardTransformR2PlanarBatched", "cufft_8cc.html#a1c2c5b25a17ec1d6d66250f11ba3400a", null ],
     [ "acFFTForwardTransformSymmetricR2C", "cufft_8cc.html#a60ac7badd6a8e10bd158609f3af30614", null ],
     [ "acFFTInit", "cufft_8cc.html#ae00d539b3a2abdd86cd9c0084324e4ce", null ],
     [ "acFFTQuit", "cufft_8cc.html#abd575c60198344f9c50fd0df51fe2392", null ],
